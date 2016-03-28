906b7986344b94bbd4d44a8f4a79f1a062be727a
Varusha <windrose88@gmail.com>
2016-03-28 13:35:07 +0300

    validate only for UA numbers

--- lib/phone_validator.rb
+++ lib/phone_validator.rb
@@ -2,6 +2,6 @@ class PhoneValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     return true unless value.present?
 
-    record.errors.add attribute, I18n.t('errors.messages.invalid') unless GlobalPhone.validate(value)
+    record.errors.add attribute, I18n.t('errors.messages.invalid') unless GlobalPhone.validate(value, :ua)
   end
 end
